a3d1dfd574602e205c916753b09414088dee3341
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2026-01-10 18:49:49 -0500

    first commit, created python/node files and associated docker files

---
+++ nodejs_example/index.js
@@ -0,0 +1,8 @@
+//Print greeting
+console.log("Hello ASL");
+
+//Get current date for output
+const date = new Date();
+const formattedDate = date.toISOString().split("T")[0];
+
+console.log(formattedDate);
---
+++ python_example/main.py
@@ -0,0 +1,7 @@
+import datetime
+
+#Print greeting
+print("Hello ASL!")
+
+#Print the current date and time
+print(datetime.date.today())
