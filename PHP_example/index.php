e80603f60c46e680d3325d96b5bdd8dd35838d1d
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2026-01-11 08:15:32 -0500

    added php and ruby examples

---
+++ PHP_example/index.php
@@ -0,0 +1,7 @@
+<?php
+// Print greeting
+echo "Hello ASL!\n";
+
+// Print the current date
+echo date("Y-m-d") . "\n";
+?>
---
+++ ruby_example/main.rb
@@ -0,0 +1,5 @@
+# Print greeting
+puts "Hello ASL!"
+
+# Print current date
+puts Time.now.strftime("%Y-%m-%d")
